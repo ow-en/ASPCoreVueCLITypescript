5b3fa45fc07f8192b0318717993d4def257eb016
owensource <ciruz001@cougars.csusm.edu>
2018-11-12 13:10:01 -0800

    Authentication in progress - ASP.Net Core Identity & IdentityServer4

--- Auth/Program.cs
+++ Auth/Program.cs
@@ -1,15 +1,14 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Server.HttpSys;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace AtlasOneCore
+namespace Auth
 {
     public class Program
     {
@@ -21,15 +20,5 @@ namespace AtlasOneCore
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
-
-        //.UseHttpSys(options => // httpsys config start
-        //{
-        //    // The following options are set to default values.
-        //    options.Authentication.Schemes = AuthenticationSchemes.None;
-        //    options.Authentication.AllowAnonymous = true;
-        //    options.MaxConnections = null;
-        //    options.MaxRequestBodySize = 30000000;
-        //    options.UrlPrefixes.Add("http://localhost:5000");
-        //});
     }
 }
